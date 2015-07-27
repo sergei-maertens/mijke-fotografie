461bd3cb8c0e32ace35e9f049acc85c8c2ab2d88
Sergei Maertens <sergei@maykinmedia.nl>
2015-07-27 23:41:40 +0200

    Added front-end deps

--- src/mijke/static/config.js
+++ src/mijke/static/config.js
@@ -18,6 +18,8 @@ System.config({
     "babel": "npm:babel-core@5.8.9",
     "babel-runtime": "npm:babel-runtime@5.8.9",
     "core-js": "npm:core-js@0.9.18",
+    "font-awesome": "npm:font-awesome@4.3.0",
+    "jquery": "github:components/jquery@2.1.4",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
@@ -28,6 +30,9 @@ System.config({
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
+    "npm:font-awesome@4.3.0": {
+      "css": "github:systemjs/plugin-css@0.1.13"
     }
   }
 });
