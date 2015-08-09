fac88503b838c10f0cd6a2cd21680be4dadb47e7
Sergei Maertens <sergei@maykinmedia.nl>
2015-08-09 20:51:09 +0200

    Correct bitters set-up

--- sass_watch.sh
+++ sass_watch.sh
@@ -2,6 +2,5 @@
 sassc -w \
     -I src/mijke/static/bower_components/bourbon/app/assets/stylesheets/ \
     -I src/mijke/static/bower_components/neat/app/assets/stylesheets/ \
-    -I src/mijke/static/bower_components/bitters/app/assets/stylesheets/ \
     src/mijke/sass/screen.scss \
-    src/mijke/static/screen.css \
+    src/mijke/static/screen.css
