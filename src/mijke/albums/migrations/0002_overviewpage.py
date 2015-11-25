a35963ff3779c8bb9a0910a0d8aab486935b936d
Sergei Maertens <sergei@maykinmedia.nl>
2015-11-25 09:20:14 +0100

    Add more page types

---
+++ src/mijke/albums/migrations/0002_overviewpage.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import wagtail.wagtailcore.blocks
+import wagtail.wagtailcore.fields
+import wagtail.wagtailimages.blocks
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('wagtailcore', '0020_add_index_on_page_first_published_at'),
+        ('albums', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='OverviewPage',
+            fields=[
+                ('page_ptr', models.OneToOneField(parent_link=True, primary_key=True, auto_created=True, to='wagtailcore.Page', serialize=False)),
+                ('body', wagtail.wagtailcore.fields.StreamField((('h2', wagtail.wagtailcore.blocks.CharBlock(classname='title', icon='title')), ('h3', wagtail.wagtailcore.blocks.CharBlock(classname='title', icon='title')), ('h4', wagtail.wagtailcore.blocks.CharBlock(classname='title', icon='title')), ('intro', wagtail.wagtailcore.blocks.RichTextBlock(icon='pilcrow')), ('paragraph', wagtail.wagtailcore.blocks.RichTextBlock(icon='pilcrow')), ('image', wagtail.wagtailcore.blocks.StructBlock((('image', wagtail.wagtailimages.blocks.ImageChooserBlock()), ('caption', wagtail.wagtailcore.blocks.RichTextBlock())), label='image', icon='image'))))),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('wagtailcore.page',),
+        ),
+    ]
--- src/mijke/albums/models.py
+++ src/mijke/albums/models.py
@@ -1,3 +1,8 @@
+from django.utils.translation import ugettext_lazy as _
+
+from wagtail.wagtailcore.blocks import (
+    CharBlock, RichTextBlock, StreamBlock, StructBlock
+)
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import StreamField
 from wagtail.wagtailadmin.edit_handlers import StreamFieldPanel
@@ -11,4 +16,31 @@ class AlbumPage(Page):
 
     content_panels = Page.content_panels + [
         StreamFieldPanel('photos'),
+    ]
+
+
+class ImageBlock(StructBlock):
+    image = ImageChooserBlock()
+    caption = RichTextBlock()
+
+
+class OverviewStreamBlock(StreamBlock):
+    h2 = CharBlock(icon="title", classname="title")
+    h3 = CharBlock(icon="title", classname="title")
+    h4 = CharBlock(icon="title", classname="title")
+    intro = RichTextBlock(icon="pilcrow")
+    paragraph = RichTextBlock(icon="pilcrow")
+    image = ImageBlock(label=_('image'), icon="image")
+
+
+class OverviewPage(Page):
+
+    """
+    Page used as tree node.
+    """
+
+    body = StreamField(OverviewStreamBlock())
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('body')
     ]
