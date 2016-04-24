3d3ca1a474ebab217b445cfb2126d3a58c6f172a
Sergei Maertens <sergei@maykinmedia.nl>
2016-04-24 15:06:31 +0200

    Add some extra fields to overview page

--- src/mijke/albums/models.py
+++ src/mijke/albums/models.py
@@ -1,15 +1,21 @@
+from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from modelcluster.fields import ParentalKey
 from wagtail.wagtailcore.blocks import (
     CharBlock, RichTextBlock, StreamBlock, StructBlock
 )
-from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.models import Orderable, Page
 from wagtail.wagtailcore.fields import StreamField
-from wagtail.wagtailadmin.edit_handlers import StreamFieldPanel
+from wagtail.wagtailadmin.edit_handlers import (
+    FieldPanel, InlinePanel, PageChooserPanel, StreamFieldPanel
+)
 from wagtail.wagtailimages.blocks import ImageChooserBlock
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 
 
 class AlbumPage(Page):
+
     photos = StreamField([
         ('photo', ImageChooserBlock()),
     ])
@@ -33,14 +39,33 @@ class OverviewStreamBlock(StreamBlock):
     image = ImageBlock(label=_('image'), icon="image")
 
 
+class OverviewPageLink(Orderable):
+    page = ParentalKey('OverviewPage', related_name='links')
+    target_page = models.ForeignKey(Page, related_name='+')
+    title = models.CharField(_('title'), max_length=255, blank=True)
+
+    panels = [
+        PageChooserPanel('target_page'),
+        FieldPanel('title'),
+    ]
+
+
 class OverviewPage(Page):
 
     """
-    Page used as tree node.
+    Page used as tree node, to distribute to other pages.
     """
+
+    cover = models.ForeignKey(
+        'wagtailimages.Image', null=True, blank=True,
+        on_delete=models.SET_NULL, related_name='+',
+        help_text=_("Main image for this page. The body will be overlaid.")
+    )
 
     body = StreamField(OverviewStreamBlock())
 
     content_panels = Page.content_panels + [
-        StreamFieldPanel('body')
+        ImageChooserPanel('cover'),
+        StreamFieldPanel('body'),
+        InlinePanel('links', label=_("page links")),
     ]
